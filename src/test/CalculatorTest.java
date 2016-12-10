eba31b6729a42dd15ba6ea3922012bc87fdbe9e8
andkoz <andkoz@onet.eu>
2016-12-10 12:17:28 +0100

    tu bylem. Test.

--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -3,6 +3,9 @@ package test;
 import main.Calculator;
 
 import org.junit.Before;
+
+import static org.junit.Assert.*;
+
 import org.junit.After;
 import org.junit.Test;
 
@@ -24,6 +27,47 @@ public class CalculatorTest {
 	@Test
 	public void test1() throws Exception {
 		System.out.println("Metoda testowa");
+		double a = 4.0;
+		double b = 7.0;
+		double result= classUnderTest.sum(a, b);
+		assertEquals("ok.", 11, result, 0.000001);
+		
+	}
+	@Test
+	public void testSubtract() throws Exception {
+		System.out.println("Metoda testowa");
+		double a = 4.0;
+		double b = 7.0;
+		double result= classUnderTest.subtract(a, b);
+		assertEquals("ok.", a-b, result, 0.000001);
+		
+	}
+	@Test
+	public void testMultiply() throws Exception {
+		System.out.println("Metoda testowa");
+		double a = 4.0;
+		double b = 7.0;
+		double result= classUnderTest.multiply(a, b);
+		assertEquals("ok.", a*b, result, 0.000001);
+		
+	}
+	@Test
+	public void testDivide() throws Exception {
+		System.out.println("Metoda testowa");
+		double a = 4.0;
+		double b = 7.0;
+		double result= classUnderTest.divide(a, b);
+		assertEquals("ok.", a/b, result, 0.000001);
+		
+	}
+	@Test (expected = IllegalArgumentException.class)
+	public void testDivideZero() throws Exception {
+		System.out.println("Metoda testowa");
+		double a = 4.0;
+		double b = 7.0;
+		double result= classUnderTest.divide(a, 0);
+		assertEquals("ok.", a/b, result, 0.000001);
+		
 	}
 
 }
